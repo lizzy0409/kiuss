ae5271848f1e36cae061a61d7c432818a0e7278a
helios <lizzy040925@gmail.com>
2022-01-15 22:11:44 +0300

    Delete static files

--- static/scripts/breakpoints.js
+++
@@ -1,8 +0,0 @@
-// Если смартфон или планшет, то добавляем специальные css-стили
-
-if (device.mobile() || device.tablet()) {
-
-  $('head').append('<link rel="stylesheet" href="/styles/breakpoints.css" type="text/css">');
-  $('head').append('<link rel="stylesheet" href="/libs/css/hamburgers.css" type="text/css">');
-
-}
